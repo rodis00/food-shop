7392532193ad9c3020813d8f453af7cc60a69cea
rodis00 <happyekkov@gmail.com>
2023-11-25 22:50:27 +0100

    add more details to items

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
+import { StarRatingComponent } from '../comonents/partials/star-rating/star-rating.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  imports: [CommonModule, StarRatingComponent],
 })
 export class HomeComponent {
   foods: Food[] = [];
