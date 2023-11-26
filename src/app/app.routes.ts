5fd7549a81067e42523c09229fa2160822bd2bdf
rodis00 <happyekkov@gmail.com>
2023-11-26 21:13:07 +0100

    add routing by tag

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
 ];
