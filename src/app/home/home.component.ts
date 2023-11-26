cc41845b6892341bbc48f48b4cab3b99133c1d12
rodis00 <happyekkov@gmail.com>
2023-11-26 19:27:14 +0100

    add search method to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
 import { StarRatingComponent } from '../components/partials/star-rating/star-rating.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,17 @@ import { StarRatingComponent } from '../components/partials/star-rating/star-rat
 export class HomeComponent {
   foods: Food[] = [];
 
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
+  constructor(private foodService: FoodService, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      if (params['searchTerm']) {
+        this.foods = this.foodService
+          .getAll()
+          .filter((food) =>
+            food.name.toLowerCase().includes(params['searchTerm'].toLowerCase())
+          );
+      } else {
+        this.foods = foodService.getAll();
+      }
+    });
   }
 }
