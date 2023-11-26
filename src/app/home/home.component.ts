863f4481869eb7004233c885f7148951bf55464a
rodis00 <happyekkov@gmail.com>
2023-11-26 21:14:50 +0100

    add tag component to the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,13 +5,14 @@ import { Food } from '../shared/models/Food';
 import { StarRatingComponent } from '../components/partials/star-rating/star-rating.component';
 import { ActivatedRoute } from '@angular/router';
 import { SearchComponent } from '../search/search.component';
+import { TagsComponent } from '../tags/tags.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [CommonModule, StarRatingComponent, SearchComponent],
+  imports: [CommonModule, StarRatingComponent, SearchComponent, TagsComponent],
 })
 export class HomeComponent {
   foods: Food[] = [];
@@ -19,11 +20,11 @@ export class HomeComponent {
   constructor(private foodService: FoodService, private route: ActivatedRoute) {
     this.route.params.subscribe((params) => {
       if (params['searchTerm']) {
-        this.foods = this.foodService
-          .getAll()
-          .filter((food) =>
-            food.name.toLowerCase().includes(params['searchTerm'].toLowerCase())
-          );
+        this.foods = this.foodService.getAllFoodsBySearchTerm(
+          params['searchTerm']
+        );
+      } else if (params['tag']) {
+        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
       } else {
         this.foods = foodService.getAll();
       }
