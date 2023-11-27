489cffc93b523cbf9f734263e0a70de03f004a3a
rodis00 <happyekkov@gmail.com>
2023-11-27 18:42:35 +0100

    add cart page component

---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../services/cart/cart.service';
+import { Cart } from '../shared/models/Cart';
+import { CartItem } from '../shared/models/CartItem';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cart-page',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './cart-page.component.html',
+  styleUrl: './cart-page.component.css',
+})
+export class CartPageComponent {
+  cart!: Cart;
+  constructor(private cartService: CartService) {
+    this.setCart();
+  }
+
+  setCart() {
+    this.cart = this.cartService.getCart();
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
+  }
+}
--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Food } from '../shared/models/Food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
 import { StarRatingComponent } from '../components/partials/star-rating/star-rating.component';
 import { TagsComponent } from '../tags/tags.component';
+import { CartService } from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -17,12 +18,19 @@ export class FoodPageComponent {
   food!: Food;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private foodService: FoodService
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
   ) {
     this.activatedRoute.params.subscribe((params) => {
       if (params['id']) {
         this.food = foodService.getFoodById(params['id']);
       }
     });
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 }
