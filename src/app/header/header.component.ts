e9710c204994b38fe977f1b7b8c218a93c4aeeff
rodis00 <happyekkov@gmail.com>
2023-11-27 18:41:46 +0100

    add cart-page route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
