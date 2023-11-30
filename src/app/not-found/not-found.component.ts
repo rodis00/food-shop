cc39c5bd383d99997290ac8690bebbba002258f7
rodis00 <happyekkov@gmail.com>
2023-11-30 22:24:40 +0100

    add not-found component

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css',
+})
+export class NotFoundComponent {
+  @Input()
+  visible: boolean = false;
+
+  @Input()
+  notFoundMessage: string = 'Nothing found!';
+
+  @Input()
+  resetLinkText: string = 'Reset';
+
+  @Input()
+  resetLinkRoute: string = '/';
+}
